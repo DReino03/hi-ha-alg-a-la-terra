b713a39eacdd182fa0497b85bf6e444bab216382
Daniel Reinosa Luque <114745664+DReino03@users.noreply.github.com>
2023-11-15 16:58:09 +0100

    millorat

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-let string = 0
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("" + (string))
+    radio.sendString("HEEY")
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showString("String")
-    radio.sendString("" + (string))
+    basic.showString(receivedString)
+    radio.sendString("hola")
+})
+basic.forever(function () {
+    let string = 0
+    basic.showString("" + (string))
 })
